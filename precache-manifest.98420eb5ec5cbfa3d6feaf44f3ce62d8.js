7be7051baa1641432939b1910d3ca3629597cc2f
younjong park <soleme@gmail.com>
2020-06-21 15:21:41 +0900

    Updates

---
+++ precache-manifest.98420eb5ec5cbfa3d6feaf44f3ce62d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f615cb46db6540f0765bca1f7c6bba",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "12dda9d0a0f1789e1176",
+    "url": "/movie_app_2020/static/css/main.7199981a.chunk.css"
+  },
+  {
+    "revision": "b0f64fb423232732c18c",
+    "url": "/movie_app_2020/static/js/2.229824db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.229824db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12dda9d0a0f1789e1176",
+    "url": "/movie_app_2020/static/js/main.e79d8c57.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
